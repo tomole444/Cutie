4fcc269ba48ef74434c3f41a74e99ec8cc5a14ec
Tommy L <tom.leyh@study.thws.de>
2024-06-25 12:29:57 +0200

    fix setup script

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,6 @@
-git clone https://github.com/tomole444/Cutie/
-cd Cutie
+#git clone https://github.com/tomole444/Cutie/
+#cd Cutie
+source /home/$USER/anaconda3/etc/profile.d/conda.sh
 conda create -n cutie python==3.10.12
 conda activate cutie
 pip install -e .
